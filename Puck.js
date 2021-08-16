6bdd780308037e7342819fd241c8dd5fa0e3307d
mroger25 <madsonmath@hotmail.com>
2021-08-16 02:09:06 -0300

    Update Puck.js
    Consegui um algoritmo de colisão válido, porém ainda vou aprimorar utilizando cálculo vetorial;
    Jogo funcional.

--- Puck.js
+++ Puck.js
@@ -12,13 +12,14 @@ export class Puck {
     this.x = this.myCanvas.canvas.width / 2;
     this.y = this.myCanvas.canvas.height / 2;
     const a = Math.random() * 360;
-    this.setMove(a);
+    const v = {};
+    v.x = Math.cos(a * (Math.PI / 180)) * this.s;
+    v.y = Math.sin(a * (Math.PI / 180)) * -this.s;
+    this.setMove(v);
   }
 
-  setMove(a) {
-    this.a = a;
-    this.xSpeed = Math.cos(a * (Math.PI / 180)) * this.s;
-    this.ySpeed = Math.sin(a * (Math.PI / 180)) * -this.s;
+  setMove(v) {
+    this.v = v;
   }
 
   paddleCollision() {
@@ -29,7 +30,18 @@ export class Puck {
     const distance = Math.sqrt(dx * dx + dy * dy);
     this.crashed = distance < c.r + p.r ? !0 : !1;
     if (this.crashed) {
-      const m = c.ySpeed / c.xSpeed;
+      const p = c.x < c.myCanvas.canvas.width / 2 ? c.lPaddle : c.rPaddle;
+      const draw_line_p_p = (a, b) => {
+        const ca = (a.y - b.y) / (a.x - b.x);
+        const f = (x) => {
+          return a.y + (x - a.x) * ca;
+        };
+        const p = { x: 0 };
+        const q = { x: c.myCanvas.canvas.width };
+        p.y = f(p.x);
+        q.y = f(q.x);
+        return ca;
+      };
       const intersection_line_circle = ({ m, R, p }) => {
         const a = m * m * p.x + p.x;
         const b = m * m + 1;
@@ -50,26 +62,28 @@ export class Puck {
         p: { x: p.x, y: p.y },
       });
       const focus = intersection_line_circle({
-        m,
+        m: c.v.y / c.v.x,
         R: p.r / 2,
         p: { x: p.x, y: p.y },
       });
       const pcrash = c.x > p.x ? crash[0] : crash[1];
       const pfocus = c.x > p.x ? focus[0] : focus[1];
-      const dx = pcrash.x - pfocus.x;
-      const dy = pcrash.y - pfocus.y;
-      const dist = Math.sqrt(dx * dx + dy * dy);
-      const ang_return = Math.acos(dx / dist) / (Math.PI / 180);
-      console.log(ang_return);
-      c.setMove(ang_return);
+      const ca = draw_line_p_p(pcrash, pfocus);
+      const new_vector = intersection_line_circle({
+        m: ca,
+        R: Math.sqrt(c.v.x * c.v.x + c.v.y * c.v.y),
+        p: { x: c.x, y: c.y },
+      });
+      const v = c.x > p.x ? new_vector[0] : new_vector[1];
+      c.setMove({ x: v.x - c.x, y: v.y - c.y });
     }
   }
 
   move() {
-    this.x += this.xSpeed;
-    this.y += this.ySpeed;
+    this.x += this.v.x;
+    this.y += this.v.y;
     if (this.y - this.r < 0 || this.y + this.r > this.myCanvas.canvas.height) {
-      this.setMove(360 - this.a);
+      this.setMove({ x: this.v.x, y: -this.v.y });
     }
     if (this.x + this.r < 0 || this.x - this.r > this.myCanvas.canvas.width) {
       if (this.x < this.myCanvas.canvas.width / 2) {
